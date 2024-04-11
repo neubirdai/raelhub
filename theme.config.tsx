9601446332906e8ca384666b8f810941600cd998
Gou Rao <gou@neubird.ai>
2024-04-11 13:38:04 -0700

    rael hub

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>RAEL Hub</span>,
   project: {
     link: 'https://github.com/neubirdai',
   },
